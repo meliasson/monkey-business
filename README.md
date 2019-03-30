9ec3e6dbdea326640a569d9330beb65c9fde89b2
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-03-30 07:20:41 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# monkey-business
+
+A Vagrant machine for _all_ development that isn't work related!!!
+
+![](evil-world-dominance-laughter.gif)
---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+# Set time zone.
+sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+
+# Install Git.
+sudo apt-get -y install git
+
+# Install Node environment.
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
