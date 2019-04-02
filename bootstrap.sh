59019ea6ac1bd1ce8a37842939c78b8aadbad806
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-04-02 19:12:27 +0200

    Configure a decent prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ sudo apt-get -y upgrade
 # Set time zone.
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
+# Fix decent prompt
+echo 'function parse_git_branch {
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+    echo "("${ref#refs/heads/}") "
+}' >> ~/.bashrc
+echo 'PS1="\[\033[48;5;244;38;5;255m\]\u@\h:\W \$(parse_git_branch)$\[\033[00m\] "' >> ~/.bashrc
+
 # Install Git.
 sudo apt-get -y install git
 
